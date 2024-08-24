7283a03c8561b8b6206241c0f900ac81132bb20b
adil <adil.aslam1@icloud.com>
2024-08-24 23:04:47 +0500

    completed migrations

---
+++ backend/app/Http/Controllers/LoginController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {
+            $user = Auth::user();
+            $token = $user->createToken('LaravelPassportToken')->accessToken;
+            return response()->json(['token' => $token], 200);
+        } else {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+    }
+}
